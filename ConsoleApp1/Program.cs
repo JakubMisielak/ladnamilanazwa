701ad494c85588c8cc31a25276dde9124c978c35
bambo <kubamisielak@wp.pl>
2024-10-16 12:46:18 +0200

    zadanie

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,21 @@
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("podaj znak")
+            string podznak = Console.ReadLine();
+        int a = 1;
         for (int i = 0; i < 10; i++)
         {
             Console.WriteLine(i);
+        }
+        while (a < 100)
+        {
+            a++;
+            Console.WriteLine(a);
+        }
+        foreach (char b in podznak)
+        {
+            Console.WriteLine(b);
         }
     }
 }
